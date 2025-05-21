e9fe20708d997c60e254e875859d7ce7cc1a4fad
munna <habiburrahmanmunna56@gmail.com>
2025-05-21 21:05:14 +0600

    feat: enhance dashboard with sales overview and new sidebar items

--- src/Components/sections/overview/statCard.jsx
+++ src/Components/sections/overview/statCard.jsx
@@ -1,12 +1,15 @@
-"use client"
-import React from 'react';
-import { motion } from 'framer-motion';
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
 
 const StatCard = ({ name, icon: Icon, value }) => {
   return (
-    <motion.div whileHover={{y:-5, boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.5"}} className='bg-[#1e1e1e] backdrop-blur-md overflow-hidden shadow-lg rounded-xl border border-[#2f2f2f]'>
-      <div className='px-4 py-5 sm:p-6'>
-        <span className='flex items-center text-sm font-medium text-gray-300'>
+    <motion.div
+      whileHover={{ y: -5, boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.5" }}
+      className="bg-[#1e1e1e] backdrop-blur-md overflow-hidden shadow-lg rounded-xl border border-[#2f2f2f]"
+    >
+      <div className="px-4 py-5 sm:p-6">
+        <span className="flex items-center text-sm font-medium text-gray-300">
           <Icon size={20} className="mr-2" />
           {name}
         </span>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-"use client"
-import React from 'react'
-import OverviewPage from './../Components/sections/overview/overviewPage';
+"use client";
+import React from "react";
+import OverviewPage from "./../Components/sections/overview/overviewPage";
 
 const page = () => {
   return (
     <div>
       <OverviewPage />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
